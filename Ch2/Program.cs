950c74a80a9b22d89ba58f8e566d3547b34bd09b
Alexey Ryumin <aryumin@aryumin.ru>
2017-09-16 13:51:53 +0300

    upd

--- Ch2/Program.cs
+++ Ch2/Program.cs
@@ -6,7 +6,7 @@ namespace Ch2
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[] { 5, 4, 55, 3, 4, 2, 3, 6, 7, 8, 4, 6 };
+            int[] arr = new int[] { 5, 4, 55, 54, 53, 52, 3, 4, 2, 3, 6, 7, 8, 4, 6 };
             arr = InsertSort.Sort(arr);
             foreach (int i in arr)
                 Console.WriteLine(i);
