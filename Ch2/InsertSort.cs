12b3258a9e5a513c813ba86d3846ebccffe24089
Alexey Ryumin <aryumin@aryumin.ru>
2017-09-16 13:07:41 +0300

    added insert sort algorythm

---
+++ Ch2/InsertSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Ch2
+{
+    /// <summary>
+    /// Сортировка вставкой. Взято из псевдокода.
+    /// </summary>
+    public static class InsertSort
+    {
+        public static int[] Sort(int[] arr){
+            
+            int key;
+            int i;
+
+            for (int j = 1; j < arr.Length; j++){
+                key = arr[j];
+                //Вставка arr[j] в отсортированную последовательность. 
+                i = j - 1;
+                while(i > -1 && arr[i] > key){
+                    arr[i + 1] = arr[i];
+                    i--;
+                }
+                arr[i + 1] = key;
+            }
+
+            return arr;
+        }
+    }
+}
--- Ch2/Program.cs
+++ Ch2/Program.cs
@@ -6,7 +6,13 @@ namespace Ch2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arr = new int[] { 5, 4, 55, 3, 4, 2, 3, 6, 7, 8, 4, 6 };
+            arr = InsertSort.Sort(arr);
+            foreach (int i in arr)
+                Console.WriteLine(i);
+
+
+            Console.ReadLine();
         }
     }
 }
